cacdca976a0857c5ab3440dccb8ddbcb8ce81575
Marcin Wierzbanowski <marcin@wierzbanowski.com>
2014-11-16 03:23:11 +0100

    pep8

--- setup.py
+++ setup.py
@@ -9,27 +9,29 @@ try:
 except ImportError:
     from distutils.core import setup, find_packages
 
-if sys.version_info < (2,6):
-    raise NotImplementedError("Sorry, you need at least Python 2.6 or Python 3.x to use jsobject.")
+if sys.version_info < (2, 6):
+    raise NotImplementedError("Sorry, you need at least Python 2.6 or Python \
+                               3.x to use jsobject.")
 
 import jsobject
 HERE = os.path.abspath(os.path.dirname(__file__))
 readme = open(os.path.join(HERE, 'README.rst')).read()
 
 setup(
-    name             = 'jsobject',
-    version          = jsobject.__version__,
-    description      = 'Jsobject is simple implementation JavaScript-Style Objects in Python.',
-    long_description = readme,
-    author           = 'Marcin Wierzbanowski',
-    author_email     = 'marcin@wierzbanowski.com',
-    url              = 'http://mavier.github.io/jsobject',
-    py_modules       = ['jsobject'],
-    packages         = find_packages(),
-    license          = 'MIT',
-    platforms        = 'any',
-    keywords         = ['jsobject', 'Object', 'json', 'chain', 'javascript'],
-    classifiers      = [
+    name='jsobject',
+    version=jsobject.__version__,
+    description='Jsobject is simple implementation JavaScript-Style Objects \
+                 in Python.',
+    long_description=readme,
+    author='Marcin Wierzbanowski',
+    author_email='marcin@wierzbanowski.com',
+    url='http://mavier.github.io/jsobject',
+    py_modules=['jsobject'],
+    packages=find_packages(),
+    license='MIT',
+    platforms='any',
+    keywords=['jsobject', 'Object', 'json', 'chain', 'javascript'],
+    classifiers=[
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
