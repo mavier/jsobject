41f01c846b1685fa508748878a8bb224ef459beb
Marcin Wierzbanowski <marcin@wierzbanowski.com>
2015-05-06 23:37:39 +0200

    add merge

--- jsobject/jsobject.py
+++ jsobject/jsobject.py
@@ -12,42 +12,19 @@ class Object(dict):
             self[k] = self.__class__(v)
         else:
             self[k] = v
-"""
-add merge
 
-from copy import deepcopy
+    def merge(self, mapping):
+        for key in mapping:
+            if key not in self:
+                self[key] = mapping[key]
+                continue;
 
-d1 = {'a': {'b':{'c':'d'}}, 'e': {'b':{'c':'d', 'e':3}}} 
-d2 = {'b': {'b':{'c':{'d':'1'}}}, 'e':{'b':{'c':{'d':"2"}}}}
-
-
-
-def merge(a, b, path=None):
-    if path is None: path = []
-    for key in b:
-        if key in a:
-            if isinstance(a[key], dict) and isinstance(b[key], dict):
-                merge(a[key], b[key], path + [str(key)])
-            elif a[key] == b[key]:
+            if isinstance(self[key], dict) and isinstance(mapping[key], dict):
+                self[key].merge(mapping[key])
+            elif isinstance(self[key], list) and isinstance(mapping[key], list):
+                self[key].extend(mapping[key])
+            elif self[key] == mapping[key]:
                 pass
             else:
-                raise Exception('Conflict at %s' % '.'.join(path + [str(key)]))
-        else:
-            a[key] = b[key]
-    return a
-
-def dict_merge(a, b):
-    if not isinstance(b, dict):
-        return b
-    result = deepcopy(a)
-    for k, v in b.iteritems():
-        if k in result and isinstance(result[k], dict):
-                result[k] = dict_merge(result[k], v)
-        else:
-            result[k] = deepcopy(v)
-    return result
-
-print dict_merge(d1, d2)
-print dict_merge(d2, d1)
-
-"""
+                self[key] = mapping[key]
+                # raise Exception('Conflict')
